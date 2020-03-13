89c6830cde6d3dbb8e6df7f14a716c2785cfc6b9
guxukai <1378026744@qq.com>
2020-03-13 09:42:32 +0800

    feat: add Button;

--- packages/components/Box/index.style.js
+++ packages/components/Box/index.style.js
@@ -43,6 +43,90 @@ export default [
                         // 最小单元显示或隐藏
                     }
                 }
+            },
+            {
+                type: 'Box',
+                name: '自由容器',
+                theme: 'theme 2',
+                level: 1,
+                pc: {
+                    units: {
+                        height: 200,
+                        width: 375
+                    },
+                    style: {
+                        backgroundColor: '#000000',
+                        borderStyle: 'solid',
+                        borderColor: '#000000',
+                        borderWidth: '1px'
+                    },
+                    class: {
+                        // 类名
+                    },
+                    visible: {
+                        // 最小单元显示或隐藏
+                    }
+                },
+                phone: {
+                    units: {
+                        height: 50,
+                        width: 280,
+                        borderWidth: 1
+                    },
+                    style: {
+                        backgroundColor: '#000000',
+                        borderStyle: 'solid',
+                        borderColor: '#000000'
+                    },
+                    class: {
+                        // 类名
+                    },
+                    visible: {
+                        // 最小单元显示或隐藏
+                    }
+                }
+            },
+            {
+                type: 'Box',
+                name: '自由容器',
+                theme: 'theme 3',
+                level: 1,
+                pc: {
+                    units: {
+                        height: 200,
+                        width: 375
+                    },
+                    style: {
+                        backgroundColor: '#87A25E',
+                        borderStyle: 'solid',
+                        borderColor: '#000000',
+                        borderWidth: '1px'
+                    },
+                    class: {
+                        // 类名
+                    },
+                    visible: {
+                        // 最小单元显示或隐藏
+                    }
+                },
+                phone: {
+                    units: {
+                        height: 50,
+                        width: 280,
+                        borderWidth: 1
+                    },
+                    style: {
+                        backgroundColor: '#87A25E',
+                        borderStyle: 'solid',
+                        borderColor: '#000000'
+                    },
+                    class: {
+                        // 类名
+                    },
+                    visible: {
+                        // 最小单元显示或隐藏
+                    }
+                }
             }
         ]
     }
--- packages/components/Button/index.style.js
+++ packages/components/Button/index.style.js
@@ -26,7 +26,7 @@ export default [
                         height: 50,
                         fontSize: 12, // 字体大小 12 - 3
                         borderWidth: 0, // 边框粗细0 - 20
-                        borderRadius: 35 // 边框弧度 任意数字
+                        borderRadius: 0 // 边框弧度 任意数字
                     },
                     style: {
                         zIndex: 999,
@@ -51,7 +51,7 @@ export default [
                         height: 50,
                         fontSize: 12, // 字体大小 12 - 3
                         borderWidth: 0, // 边框粗细0 - 20
-                        borderRadius: 35 // 边框弧度 任意数字
+                        borderRadius: 0 // 边框弧度 任意数字
                     },
                     style: {
                         zIndex: 999,
@@ -61,6 +61,144 @@ export default [
                         background: '#000000', // 背景颜色
                         borderStyle: 'solid', // 边框样式
                         borderColor: '#efefef', // 边框颜色
+                        justifyContent: 'center' // 文字位置  center/left/right
+                    },
+                    class: {
+                        // 类名
+                    },
+                    visible: {
+                        // 最小单元显示或隐藏
+                    }
+                }
+            },
+            {
+                type: 'Button',
+                theme: 'Theme 2',
+                name: '按钮',
+                level: 2,
+                data: {
+                    text: {
+                        cn: '按钮名称',
+                        en: 'button name'
+                    },
+                    link: {
+                        linkType: 1,
+                        linkId: null,
+                        linkTitle: '',
+                        path: 'javascript:;'
+                    },
+                    target: false
+                },
+                pc: {
+                    units: {
+                        width: 140,
+                        height: 50,
+                        fontSize: 12, // 字体大小 12 - 3
+                        borderWidth: 1, // 边框粗细0 - 20
+                        borderRadius: 0 // 边框弧度 任意数字
+                    },
+                    style: {
+                        zIndex: 999,
+                        display: 'flex',
+                        alignItems: 'center',
+                        color: '#000000', // 字体颜色
+                        background: '#ffffff', // 背景颜色
+                        borderStyle: 'solid', // 边框样式
+                        borderColor: '#000000', // 边框颜色
+                        justifyContent: 'center' // 文字位置  center/left/right
+                    },
+                    class: {
+                        // 类名
+                    },
+                    visible: {
+                        // 最小单元显示或隐藏
+                    }
+                },
+                phone: {
+                    units: {
+                        width: 200,
+                        height: 50,
+                        fontSize: 12, // 字体大小 12 - 3
+                        borderWidth: 1, // 边框粗细0 - 20
+                        borderRadius: 0 // 边框弧度 任意数字
+                    },
+                    style: {
+                        zIndex: 999,
+                        display: 'flex',
+                        alignItems: 'center',
+                        color: '#000000', // 字体颜色
+                        background: '#ffffff', // 背景颜色
+                        borderStyle: 'solid', // 边框样式
+                        borderColor: '#000000', // 边框颜色
+                        justifyContent: 'center' // 文字位置  center/left/right
+                    },
+                    class: {
+                        // 类名
+                    },
+                    visible: {
+                        // 最小单元显示或隐藏
+                    }
+                }
+            },
+            {
+                type: 'Button',
+                theme: 'Theme 3',
+                name: '按钮',
+                level: 2,
+                data: {
+                    text: {
+                        cn: '按钮名称',
+                        en: 'button name'
+                    },
+                    link: {
+                        linkType: 1,
+                        linkId: null,
+                        linkTitle: '',
+                        path: 'javascript:;'
+                    },
+                    target: false
+                },
+                pc: {
+                    units: {
+                        width: 140,
+                        height: 50,
+                        fontSize: 12, // 字体大小 12 - 3
+                        borderWidth: 0, // 边框粗细0 - 20
+                        borderRadius: 0 // 边框弧度 任意数字
+                    },
+                    style: {
+                        zIndex: 999,
+                        display: 'flex',
+                        alignItems: 'center',
+                        color: '#000000', // 字体颜色
+                        background: '#668F99', // 背景颜色
+                        borderStyle: 'solid', // 边框样式
+                        borderColor: '#ffffff', // 边框颜色
+                        justifyContent: 'center' // 文字位置  center/left/right
+                    },
+                    class: {
+                        // 类名
+                    },
+                    visible: {
+                        // 最小单元显示或隐藏
+                    }
+                },
+                phone: {
+                    units: {
+                        width: 200,
+                        height: 50,
+                        fontSize: 12, // 字体大小 12 - 3
+                        borderWidth: 0, // 边框粗细0 - 20
+                        borderRadius: 0 // 边框弧度 任意数字
+                    },
+                    style: {
+                        zIndex: 999,
+                        display: 'flex',
+                        alignItems: 'center',
+                        color: '#000000', // 字体颜色
+                        background: '#668F99', // 背景颜色
+                        borderStyle: 'solid', // 边框样式
+                        borderColor: '#ffffff', // 边框颜色
                         justifyContent: 'center' // 文字位置  center/left/right
                     },
                     class: {
